98f14fa384e0af782baf28b6bcada826bc776cae
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-01-16 09:37:59 -0300

    reorganizando repositorios

---
+++ Prova_cs/Prova/Principal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prova.Parte_1
+
+{
+	public class Principal
+	{
+		static void Main(string[] args)
+		{
+			List<Pessoa> pessoas = new List<Pessoa>();
+			pessoas.Add(new Pessoa("Joao", 15));
+			pessoas.Add(new Pessoa("Leandro", 21));
+			pessoas.Add(new Pessoa("Paulo", 17));
+			pessoas.Add(new Pessoa("Jessica", 18));
+
+			int maiorIdade = -1;
+			string maisVelha = "";
+			int idadeJessica = -1;
+
+			for (int i = 0; i < pessoas.Count; i++)
+			{
+				if ((pessoas[i].idade) > maiorIdade)
+				{
+					maiorIdade = pessoas[i].idade;
+					maisVelha = pessoas[i].nome;
+				}
+			}
+			Console.WriteLine("A pessoa mais velha é: {0}", maisVelha);
+
+			Console.WriteLine("A lista possui {0} pessoas", pessoas.Count);
+
+			for (int i = 0; i < pessoas.Count; i++)
+			{
+				if (pessoas[i].idade < 18)
+				{
+					pessoas.RemoveAt(i);
+				}
+
+			}
+			Console.WriteLine("A lista possui {0} pessoas", pessoas.Count);
+
+			for (int i = 0; i < pessoas.Count; i++)
+			{
+				if (pessoas[i].nome == "Jessica")
+				{
+					idadeJessica = pessoas[i].idade;
+				}
+			}
+			Console.WriteLine("A Jessica está na lista e tem {0} anos", idadeJessica);
+
+
+
+
+		}
+	}
+	public class Pessoa
+	{
+		public string nome { get; set; }
+		public int idade;
+
+		public Pessoa(String nome, int idade)
+		{
+			this.nome = nome;
+			this.idade = idade;
+		}
+	}
+}
---
+++ Prova_cs/Prova_parte2/Principal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prova.Parte_2
+{
+
+	public class Principal
+	{
+		static void Main(string[] args)
+		{
+		Gerente p1 = new Gerente("Ricardo", 30, 25000);
+		Supervisor p2 = new Supervisor("Silvana", 55, 10000);
+		Vendedor p3 = new Vendedor("Mauro", 62, 8000);
+		Gerente p4 = new Gerente("Rodrigo", 20, 30000);
+		Vendedor p5 = new Vendedor("Samara", 47, 7000);
+		}
+
+	}
+
+	public abstract class Funcionario
+	{
+		private string nome;
+		private int idade;
+		private float salario;
+
+		public Funcionario(string nome, int idade, float salario)
+		{
+			this.nome = nome;
+			this.idade = idade;
+			this.salario = salario;
+
+			Console.WriteLine("{0} tem {1} anos e bonificação de R${2:0,000.00}",nome,idade,bonificacao());
+		}
+
+		public virtual float bonificacao()
+		{
+			return this.salario;
+		}
+	}
+
+	public class Gerente : Funcionario
+	{
+		public Gerente(string nome, int idade, float salario) : base(nome, idade, salario)
+		{
+
+		}
+
+		public override float bonificacao()
+		{
+			return base.bonificacao() + 10000.00f;
+		}
+	}
+
+	public class Supervisor : Funcionario
+	{
+		public Supervisor(string nome, int idade, float salario) : base(nome, idade, salario)
+		{
+
+		}
+
+		public override float bonificacao()
+		{
+			return base.bonificacao() + 5000.00f;
+		}
+	}
+
+	public class Vendedor : Funcionario
+	{
+		public Vendedor(string nome, int idade, float salario) : base(nome, idade, salario)
+		{
+
+		}
+
+		public override float bonificacao()
+		{
+			return base.bonificacao() + 3000.00f;
+		}
+	}
+}
+
