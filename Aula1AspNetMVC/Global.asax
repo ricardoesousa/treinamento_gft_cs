decf1353a67ec3c1facc6819b4f3216e6b547c1a
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-01-20 14:47:59 -0300

    Incluindo DesafioMattos e primeira aula de MVC

---
+++ Aula1AspNetMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Aula1AspNetMVC.MvcApplication" Language="VB" %>
---
+++ DesafioMattos/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace DesafioMattos
+{
+    public class Principal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecione uma linguagem:");
+            Console.WriteLine("'en' para English");
+            Console.WriteLine("'sp' para Spanish");
+            Console.WriteLine("'de' para German");
+
+            string language = Console.ReadLine();
+
+            FabricaAlo fabrica = new FabricaAlo();
+
+            AloMundo a = fabrica.CriaAloMundo(language);
+
+            a.falaAlo();
+        }
+    }
+
+    public interface AloMundo
+    {
+        public void falaAlo();
+    }
+
+    public class English : AloMundo
+    {
+        public void falaAlo()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
+    public class Spanish : AloMundo
+    {
+        public void falaAlo()
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+
+    public class German : AloMundo
+    {
+        public void falaAlo()
+        {
+            Console.WriteLine("Hallo Welt");
+        }
+    }
+
+    public class FabricaAlo
+    {
+        public AloMundo CriaAloMundo(string language)
+        {
+            switch (language)
+            {
+                case "en":
+                    return new English();
+                case "sp":
+                    return new Spanish();
+                case "de":
+                    return new German();
+                default:
+                    return null;
+            }
+        }
+    }
+}
+
+
