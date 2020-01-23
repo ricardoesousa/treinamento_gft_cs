b3a9e7e65cd0b4396775ee9b0a316235fbf53dd1
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-01-23 15:00:59 -0300

    Inclusão Desafio05

---
+++ Desafio05_cs/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Desafio05_cs.Models;
+
+namespace Desafio05_cs.Controllers
+    {
+    public class HomeController : Controller
+        {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController ( ILogger<HomeController> logger )
+            {
+            _logger = logger;
+            }
+
+        public IActionResult Index ( )
+            {
+            return View ();
+            }
+
+        public IActionResult Projects ( )
+            {
+            return View ();
+            }
+
+        public IActionResult Contact ( )
+            {
+            return View ();
+            }
+
+        public IActionResult Skills ( )
+            {
+            return View ();
+            }
+
+        [ResponseCache ( Duration = 0, Location = ResponseCacheLocation.None, NoStore = true )]
+        public IActionResult Error ( )
+            {
+            return View ( new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier } );
+            }
+        }
+    }
