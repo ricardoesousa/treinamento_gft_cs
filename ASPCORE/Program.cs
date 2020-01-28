f2b53b7bd56a81881550ca6cb94a90ad894401dc
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-01-28 14:44:45 -0300

    teste

---
+++ ASPCORE/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ASPCORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool helloWord = false;
+
+            if (helloWord)
+            {
+                System.Console.WriteLine("olá mundo - HelloWorld");
+            }
+            else
+            {
+                System.Console.WriteLine("Bye World");
+            }
+            if (helloWord)
+            {
+                System.Console.WriteLine("dfpse");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
