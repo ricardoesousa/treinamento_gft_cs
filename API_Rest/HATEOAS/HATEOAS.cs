3f308ea5c1ff2760bdd17a39ef1b9e59eefbdb1d
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-03-04 14:25:35 -0300

    API REST HATEAOS criando classe de HATEAOS

---
+++ API_Rest/HATEOAS/HATEOAS.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace API_Rest.HATEOAS
+{
+    public class HATEOAS
+    {
+        private string url;
+        private string protocol = "https://";
+        public List<Link> actions = new List<Link>();
+
+        public HATEOAS(string url)
+        {
+            this.url = url;
+        }
+
+        public HATEOAS(string url, string protocol)
+        {
+            this.url = url;
+            this.protocol = protocol;
+        }
+
+        public void AddAction(string rel, string method)
+        {
+            actions.Add(new Link(this.protocol + this.url, rel, method));
+        }
+
+        public Link[] GetActions()
+        {
+            return actions.ToArray();
+        }
+    }
+}
