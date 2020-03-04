183f866b37ed61198c6b2235571356f8aeaefe11
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-03-04 10:54:28 -0300

    API REST versionamento

--- API_Rest/Controllers/ProdutosController.cs
+++ API_Rest/Controllers/ProdutosController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API_Rest.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ProdutosController : ControllerBase
 
