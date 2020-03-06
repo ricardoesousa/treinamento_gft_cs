c6fb19c9ca851a2dd68f0a8e343f70e735e6dfcc
Ricardo Sousa <ricardoesousa@hotmail.com>
2020-03-06 10:58:38 -0300

    API REST JWT gerando token

--- API_Rest/Controllers/UsuariosController.cs
+++ API_Rest/Controllers/UsuariosController.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Linq;
+using System.Text;
 using API_Rest.Data;
 using API_Rest.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace API_Rest.Controllers
 {
@@ -37,24 +40,35 @@ namespace API_Rest.Controllers
                 {
                     if (usuario.Senha.Equals(credenciais.Senha))
                     {
-                        return Ok("Logado");
+                        string chaveDeSeguranca = "school_of_net_manda_muito_bem!";
+                        var chaveSimetrica = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(chaveDeSeguranca));
+                        var credenciaisDeAcesso = new SigningCredentials(chaveSimetrica, SecurityAlgorithms.HmacSha256Signature);
+
+                        var JWT = new JwtSecurityToken(
+                            issuer: "ricardo",
+                            expires: DateTime.Now.AddHours(1),
+                            audience: "usuario_comum",
+                            signingCredentials: credenciaisDeAcesso
+                        );
+                        return Ok (new JwtSecurityTokenHandler().WriteToken(JWT));
+
                     }
                     else
                     {
                         Response.StatusCode = 401;
-                        return new ObjectResult("");
+                        return new ObjectResult("a");
                     }
                 }
                 else
                 {
                     Response.StatusCode = 401;
-                    return new ObjectResult("");
+                    return new ObjectResult("b");
                 }
             }
             catch (Exception e)
             {
                 Response.StatusCode = 401;
-                return new ObjectResult("");
+                return new ObjectResult("c");
             }
 
         }
